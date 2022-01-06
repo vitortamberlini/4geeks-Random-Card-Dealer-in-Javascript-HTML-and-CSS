7ea3f004cca97304697c5e052465034a3727e5ec
Vitor Tamberlini <vitor.tamberlini@outlook.com>
2022-01-06 01:21:46 +0000

    WIP Almost there

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 /* eslint-disable */
 
+function random_suit() {
+  return Math.floor(Math.random() * 4);
+}
+
+function random_number() {}
+
+function random_card() {
+  const suit = random_suit();
+  const number = random_number();
+}
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
