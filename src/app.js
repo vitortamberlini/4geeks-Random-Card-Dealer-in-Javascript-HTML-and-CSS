10d528c2005bd8f03ad89cdbf5365332f383066f
Vitor Tamberlini <vitor.tamberlini@outlook.com>
2022-01-09 21:27:09 +0000

    Finished project

--- src/app.js
+++ src/app.js
@@ -1,17 +1,60 @@
 /* eslint-disable */
 
-function random_suit() {
-  return Math.floor(Math.random() * 4);
+function _random_choice(array) {
+  return array[Math.floor(Math.random() * array.length)];
 }
 
-function random_number() {}
+function random_suit() {
+  const suits = ["spades", "hearts", "diamonds", "clubs"];
+
+  return _random_choice(suits);
+}
+
+function random_rank() {
+  const ranks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  return _random_choice(ranks);
+}
 
 function random_card() {
   const suit = random_suit();
-  const number = random_number();
+  const rank = random_rank();
+
+  card = {
+    suit: suit,
+    rank: rank
+  };
+
+  return card;
+}
+
+function generate_random_card() {
+  const card = random_card();
+
+  document
+    .querySelectorAll("#suits-top, #suits-bottom")
+    .forEach(element => (element.classList = `suits ${card.suit}`));
+
+  document.querySelector("#card-rank").textContent = card.rank;
 }
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generate_random_card();
+  document
+    .querySelector("#shuffle-button")
+    .addEventListener("click", generate_random_card);
 };
